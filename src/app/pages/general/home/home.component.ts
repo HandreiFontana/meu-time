b48b5e8e78e736618cc0566c04bb18e2b8a42743
HandreiFontana <HandreiFontana>
2023-05-24 10:52:25 -0300

    feat: fake leagues added

--- src/app/pages/general/home/home.component.ts
+++ src/app/pages/general/home/home.component.ts
@@ -19,6 +19,16 @@ export class HomeComponent {
     { label: 'França', action: this.countryOptionClick.bind(this) },
   ]
 
+  public leagueSelected?: ExpansibleCardOption
+  public leagues: ExpansibleCardOption[] = [
+    { label: 'Bundesliga', action: this.leagueOptionClick.bind(this) },
+    { label: 'Serie A', action: this.leagueOptionClick.bind(this) },
+    { label: 'La Liga', action: this.leagueOptionClick.bind(this) },
+    { label: 'Premier League', action: this.leagueOptionClick.bind(this) },
+    { label: 'Brasileirão', action: this.leagueOptionClick.bind(this) },
+    { label: 'Ligue 1', action: this.leagueOptionClick.bind(this) },
+  ]
+
   public toggleExpandedCard(card: string) {
     this.expandedCard = card !== this.expandedCard ? card : null
   }
@@ -30,5 +40,10 @@ export class HomeComponent {
   private countryOptionClick(countryOption: ExpansibleCardOption) {
     this.countrySelected = countryOption
     this.expandedCard = 'leagues'
+  }
+
+  private leagueOptionClick(leagueOption: ExpansibleCardOption) {
+    this.leagueSelected = leagueOption
+    this.expandedCard = 'teams'
   }
 }
