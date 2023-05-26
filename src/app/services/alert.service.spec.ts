c60fce023093d88e453491e42cc8586697257ea7
HandreiFontana <HandreiFontana>
2023-05-26 17:06:24 -0300

    feat: alert service tests updated

--- src/app/services/alert.service.spec.ts
+++ src/app/services/alert.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 
 import { AlertService } from './alert.service';
 
@@ -7,10 +7,28 @@ describe('AlertService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(AlertService);
+    service = TestBed.inject(AlertService);;
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should emit success message', fakeAsync (() => {
+    const message = 'message'
+    service.success(message)
+
+    expect(service.type).toBe('success')
+    expect(service.text).toBe(message)
+    expect(service.isActive).toBeTrue()
+
+    tick(6000)
+    expect(service.isActive).toBeFalse()
+  }))
+
+  it('should close the alert message', () => {
+    service.closeAlert()
+
+    expect(service.isActive).toBeFalse()
+  })
 });
